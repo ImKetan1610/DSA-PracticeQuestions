d7d57eceb1984ff3679c2cbbc5090f2256a1f4e7
ImKetan1610 <ketan.makode@gmail.com>
2022-12-05 03:00:13 +0530

    map symbol

---
+++ Folder1/map_symbol.js
@@ -0,0 +1,59 @@
+/*
+
+Problem Statement:
+You are given a number stored in a variable with the name N
+You have to map the following symbols starting from the value N
+The mapping is such that the difference between the values of two characters, is 2
+
+'!','@','#','$','%','^','&','*'
+
+For example, consider the value stored inN = 10, then the required output will be
+!-10
+@-12
+#-14
+$-16
+%-18
+^-20
+&-22
+*-24
+
+
+Input:
+The first and the only line of the input contains the value stored in N
+
+Output:
+Print the mapping for the symbols starting from N, as shown in the problem statement
+
+Sample Input:
+20
+
+Sample Output:
+!-20
+@-22
+#-24
+$-26
+%-28
+^-30
+&-32
+*-34
+
+Hint:
+In the sample test case, the value stored inN = 20. Therefore, the required output will be
+!-20
+@-22
+#-24
+$-26
+%-28
+^-30
+&-32
+*-34
+
+*/
+
+function mapSymbols(N) {
+  let sym = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  for (let i = 0; i < sym.length; i++) {
+    console.log(sym[i] + "-" + N);
+    N = N + 2;
+  }
+}
