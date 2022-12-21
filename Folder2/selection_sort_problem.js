be6027bc0f72980153095b821422de33f9a463cf
ImKetan1610 <ketan.makode@gmail.com>
2022-12-21 17:42:24 +0530

    selection sort problem

---
+++ Folder2/selection_sort_problem.js
@@ -0,0 +1,42 @@
+/*
+
+Problem Statement:
+You are given an array of N unsorted numbers. 
+Your task is to write SELECTION SORT such that numbers present in the array gets sorted.
+USING ANY OTHER ALGORITHM OR USING IN BUILT SORT FUNCTION WOULD LEAD TO YOUR DISQUALIFICATION
+
+
+Input:
+Input Format:
+First line of input contains N
+Second line of input contains N numbers
+
+Constraints::
+N < 500
+
+Output:
+Output the numbers given after sorting it in increasing order
+
+Sample Input:
+5
+3 5 0 9 8
+
+Sample Output:
+0 3 5 8 9
+
+*/
+
+function solve(N, arr) {
+  for (let i = 0; i < N - 1; i++) {
+    let index = i;
+    for (let j = i + 1; j < N; j++) {
+      if (arr[j] < arr[index]) {
+        index = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[index];
+    arr[index] = temp;
+  }
+  console.log(arr.join(" "));
+}
