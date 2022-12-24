da0ddb8393186858af890eb5277d52ed682dee68
ImKetan1610 <ketan.makode@gmail.com>
2022-12-24 18:17:16 +0530

    Power of 4

---
+++ Folder3/Power_of_4.js
@@ -0,0 +1,31 @@
+/*
+
+Problem Statement:
+You are given a number stored in a variable with the name, nums
+You have to find the value of the number raised to the power of 4
+For example, if the value stored in num = 3. 
+    Therefore, the value of 3 raised to the power of 4, equals3 * 3 * 3 * 3 = 81, which is the required answer
+Therefore, the output is 81
+
+Input:
+The first and the only line of the input contains the value stored in num
+
+Output:
+You have to print the value of the number, stored in num, raised to the power of 4
+
+Sample Input:
+3
+
+Sample Output:
+81
+
+Hint:
+In the sample test case, the value stored in num = 3. 
+    Therefore, the value of 3 raised to the power of 4, means3 * 3 * 3 * 3 = 81, which is the required answer
+
+*/
+
+function powerOf4(num) {
+  // Write code here
+  console.log(num ** 4);
+}
