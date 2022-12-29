335625d4e495d498db13259e0fb3bf1badb77e82
ImKetan1610 <ketan.makode@gmail.com>
2022-12-29 13:21:52 +0530

    Buy Tickets

---
+++ Folder4/buy_tickets.js
@@ -0,0 +1,81 @@
+/*
+
+Description:
+There is a long waiting line of integers waiting for their turn to buy ticket for a cricket match.
+There are N operations that can be performed on this line. The operations are of following type:
+E x : Integer x enters the queue (For such operation, print the new length of the queue.)
+D: The integer at front buys ticket and leaves the queue (For such operations, print the element that left the queue and the new size of the queue separated by space. If there is no element in the queue then print -1 in place of deleted element.)
+
+Input:
+Input Format :
+First line consists of a single integer denoting N
+Each of the following N lines contain one of the operation as described above.
+
+Constraints :
+1<=N<=100
+
+Output:
+For each enqueue operation print the new size of the queue.
+And for each dequeue operation print two integers, 
+    deleted element (-1, if queue is empty) and the new size of the queue.
+
+Sample Input:
+5
+E 2
+D
+D
+E 3
+D
+
+Sample Output:
+1
+2 0
+-1 0
+1
+3 0
+
+*/
+
+function runProgram(input) {
+  input = input.split("\n");
+  var T = +input[0];
+  var box = [];
+  for (var i = 1; i <= T; i++) {
+    var a = input[i].split(" ");
+    if (a[0] == "E") {
+      box.push(+a[1]);
+      console.log(box.length);
+    } else if (a[0] == "D") {
+      if (box.length !== 0) {
+        var ticket = box[0];
+        box.shift();
+        console.log(ticket, box.length);
+      } else {
+        console.log(-1, box.length);
+      }
+    } else {
+      console.log(-1, box.length);
+    }
+  }
+}
+
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
