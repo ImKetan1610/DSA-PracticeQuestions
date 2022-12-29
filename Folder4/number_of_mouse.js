66dd6b0297c9abf9065e34c11c56ce028932915e
ImKetan1610 <ketan.makode@gmail.com>
2022-12-29 12:45:38 +0530

    number of mouse

---
+++ Folder4/number_of_mouse.js
@@ -0,0 +1,34 @@
+/*
+
+Description:
+You are given an string of size N, of characters from 'a' to 'z'.
+You need to print number of times you can make "mouse", using characters from string. 
+You can use a character only once from the string.
+[UseKey-Value pair based data structure]
+
+Input:
+First line contains T, no of test cases.
+First line of each test case contains N, size of string
+Second line of each test case contains string S.
+
+Constraints:
+1 <= T <= 10
+1 <= N <= 10^6
+
+Output:
+For each test case, print number of times you can make "mouse"
+
+Sample Input:
+2
+7
+mousems
+8
+wimousee
+
+Sample Output:
+1
+1
+
+*/
+
+
