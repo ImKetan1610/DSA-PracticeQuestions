2270ebff769ebc08361abc3308988571b1d2203f
ImKetan1610 <ketan.makode@gmail.com>
2022-12-30 19:59:23 +0530

    pattern printing III

---
+++ Folder4/pattern_printing_inverted_c.js
@@ -0,0 +1,46 @@
+/*
+
+Description:
+You are given a number stored in a variable with the name N
+You have to print the pattern as shown below according to the value stored in N
+For example, consider the value stored inN = 5, then the required pattern will be
+* * * * *
+        *
+        *
+        *
+* * * * *
+Note : Please check for spaces
+
+Input:
+The first and the only line of the input contains the value stored in the variable N
+
+Output:
+Print the pattern as shown in the problem statement, according to the value stored in N
+
+Sample Input:
+5
+
+Sample Output:
+* * * * *
+        *
+        *
+        *
+* * * * *
+
+Hint:
+In the sample test case, the value stored inN = 5, then the required pattern will be
+* * * * *
+        *
+        *
+        *
+* * * * *
+
+The following pattern uses_to show the presence of spaces in the pattern required. 
+These are shown only for understanding, and should not be printed in the actual program
+* * * * *
+________*
+________*
+________*
+* * * * *
+
+*/
