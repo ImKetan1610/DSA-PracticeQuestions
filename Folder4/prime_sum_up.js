f9089efaf29972aedc628507345f89ad3a67c1ef
ImKetan1610 <ketan.makode@gmail.com>
2022-12-30 19:49:32 +0530

    Prime Sum Up

---
+++ Folder4/prime_sum_up.js
@@ -0,0 +1,30 @@
+/*
+
+Description:
+You are given a number, stored in a variable with the name num. 
+Find out the sum of all prime numbers in the range of [1,num], including the value stored in num
+
+Input:
+The first and the only line of the input contains the value stored in num
+
+Output:
+Print the sum of all prime numbers in the range of[1,num], including num itself
+
+Sample Input:
+13
+
+Sample Output:
+41
+
+Hint:
+In the sample test case, the value stored in num = 13. 
+Therefore, all the prime numbers, in the range of[1,num]are
+2
+3
+5
+7
+11
+13
+Therefore, the sum of prime numbers becomes,2 + 3 + 5 + 7 + 11 + 13 = 41, which is the required answer
+
+*/
