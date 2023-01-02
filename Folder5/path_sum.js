cc7e122c2aed1db4a97b94d26a6e3d0dbfd877ab
ImKetan1610 <ketan.makode@gmail.com>
2023-01-02 15:28:11 +0530

    Path Sum

---
+++ Folder5/path_sum.js
@@ -0,0 +1,62 @@
+/*
+
+Description
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+You have to complete the function below.You'll be given the pointer to the root of the binary tree and the sum.There's no need to take any input. Return the boolean true if there exists such path and false if otherwise.
+
+Input
+This is a function complete problem. There's no need to take any input.
+The input given below is for your understanding-
+The first line contains the number of nodes in the tree (t)
+Next t lines contain a node A[i]
+Next line contains the sum
+t <= 100
+A[i] <= 1000
+
+Output
+Complete the function
+
+Sample Input 1 
+5
+5
+6
+4
+7
+2
+9
+
+Sample Output 1
+True
+
+*/
+
+// Definition of Binary Tree Node
+// class Node {
+//     constructor(val) {
+//         this.val = val;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+// Complete the function below
+
+var hasPathSum = function (root, targetSum) {
+  return checkTree(root, targetSum, 0);
+
+  function checkTree(root, targetSum, sum) {
+    if (!root) {
+      return false;
+    }
+
+    sum += root.val;
+
+    if (root.left === null && root.right === null) {
+      return sum === targetSum;
+    }
+
+    return (
+      checkTree(root.left, targetSum, sum) ||
+      checkTree(root.right, targetSum, sum)
+    );
+  }
+};
